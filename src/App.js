d772285f8fb66f8438173e0b3c920eadad77ed96
IbrahimCausevic <icausevic25@gmail.com>
2023-05-18 01:42:43 +0200

    tasbeeh-counter

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends React.Component {
+  state = {
+    number: 0,
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div className="border">
+            <h1>{this.state.number}</h1>
+            <div className="flex">
+              <button
+                className="count"
+                onClick={() => {
+                  this.setState({ number: this.state.number + 1 });
+                }}
+              ></button>
+              <button
+                className="reset"
+                onClick={() => this.setState({ number: 0 })}
+              ></button>{" "}
+            </div>
+          </div>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
